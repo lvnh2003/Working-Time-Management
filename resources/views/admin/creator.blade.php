8810650318ef7591941395cbc4ecef121e8d1711
lvnh2003 <lanhnn.21it@vku.udn.vn>
2023-06-24 17:10:07 +0700

    done

--- resources/views/admin/creator.blade.php
+++ resources/views/admin/creator.blade.php
@@ -75,41 +75,6 @@
                 <!-- end row -->
             </div>
         </div>
-        <footer class="footer">
-            <div class="container-fluid">
-                <nav class="pull-left">
-                    <ul>
-                        <li>
-                            <a href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Company
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Portofolio
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Blog
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <p class="copyright pull-right">
-                    ©
-                    <script>
-                        document.write(new Date().getFullYear())
-                    </script>2023
-                    <a href="http://www.creative-tim.com"> Creative Tim </a>, made with love for a better web
-                </p>
-            </div>
-        </footer>
     </div>
 @endsection
 @push('js')
--- resources/views/admin/customer/index.blade.php
+++ resources/views/admin/customer/index.blade.php
@@ -2,8 +2,53 @@
 @section('title')
 クライアントを作り
 @endsection
+@push('css')
+<style>
+    html {
+    height: 100%;
+    /* max-height: 600px; */
+    width: 100%;
+    background-color: hsla(200,40%,30%,.4);
+    background-image:    
+      url('https://78.media.tumblr.com/cae86e76225a25b17332dfc9cf8b1121/tumblr_p7n8kqhmud1uy4lhuo1_540.png'), 
+      url('https://78.media.tumblr.com/66445d34fe560351d474af69ef3f2fb0/tumblr_p7n908e1jb1uy4lhuo1_1280.png'),
+      url('https://78.media.tumblr.com/8cd0a12b7d9d5ba2c7d26f42c25de99f/tumblr_p7n8kqhmud1uy4lhuo2_1280.png'),
+      url('https://78.media.tumblr.com/5ecb41b654f4e8878f59445b948ede50/tumblr_p7n8on19cv1uy4lhuo1_1280.png'),
+      url('https://78.media.tumblr.com/28bd9a2522fbf8981d680317ccbf4282/tumblr_p7n8kqhmud1uy4lhuo3_1280.png');
+    background-repeat: repeat-x;
+    background-position: 
+      0 20%,
+      0 100%,
+      0 50%,
+      0 100%,
+      0 0;
+    background-size: 
+      2500px,
+      800px,
+      500px 200px,
+      1000px,
+      400px 260px;
+    animation: 50s para infinite linear;
+    }
+  
+  @keyframes para {
+    100% {
+      background-position: 
+        -5000px 20%,
+        -800px 95%,
+        500px 50%,
+        1000px 100%,
+        400px 0;
+      }
+    }
+    body{
+        background-color: transparent;
+        
+    }
+  </style>          
+@endpush
 @section('content')
-    <div class="full-page lock-page" filter-color="black" data-image="../../assets/img/lock.jpeg">
+    <div class="full-page lock-page" filter-color="black" >
         <!--   you can change the color of the filter page using: data-color="blue | green | orange | red | purple" -->
         <div class="content">
             <form method="POST" action="{{ route('admin.customer.store') }}">
@@ -59,7 +104,7 @@
             </form>
         </div>
 
-        <div class="full-page-background" style="background-image: url(../../assets/img/lock.jpeg) "></div>
+        {{-- <div class="full-page-background" style="background-image: url(../../assets/img/lock.jpeg) "></div> --}}
     </div>
 @endsection
 @push('js')
